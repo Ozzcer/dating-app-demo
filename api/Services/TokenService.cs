73203aa59fc6b832319a5e31b8fa66e11cf525a9
Oz <enquiries@ozharrison.me>
2024-02-08 14:17:49 +0000

    CreateToken function added

--- api/Services/TokenService.cs
+++ api/Services/TokenService.cs
@@ -1,12 +1,30 @@
-﻿using api.Entities;
+﻿using System.Security.Claims;
+using System.Text;
+using api.Entities;
 using api.Interfaces;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
 
 namespace api.Services;
 
-public class TokenService : ITokenService
+public class TokenService(IConfiguration configuration) : ITokenService
 {
+  private readonly SymmetricSecurityKey key = new(Encoding.UTF8.GetBytes(configuration["key"] ?? throw new InvalidOperationException("Missing token security key.")));
   public string CreateToken(User user)
   {
-    throw new NotImplementedException();
+    var claims = new List<Claim>
+    {
+      new(JwtRegisteredClaimNames.NameId, user.Username)
+    };
+    var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+    var tokenDescriptor = new SecurityTokenDescriptor
+    {
+      Subject = new ClaimsIdentity(claims),
+      Expires = DateTime.Now.AddDays(1),
+      SigningCredentials = creds,
+    };
+    var tokenHandler = new JsonWebTokenHandler();
+    var token = tokenHandler.CreateToken(tokenDescriptor);
+    return token;
   }
 }
